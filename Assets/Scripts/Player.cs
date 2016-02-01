cdc1acae212386de1a6f1a1597bdea80d2c89bf0
Momin Khan <foolmoron@gmail.com>
2016-02-01 01:40:57 -0500

    dumb music tweaks for webgl

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    IEnumerator Start() {
+        var audio = GetComponent<AudioSource>();
+        audio.PlayOneShot(audio.clip);
+        while (true) {
+            yield return new WaitForSeconds(audio.clip.length);
+            audio.PlayOneShot(audio.clip);
+        }
+    }
+}
