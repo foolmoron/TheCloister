97e465aada865561edc5f256a2e857a0c42323ff
Momin Khan <foolmoron@gmail.com>
2019-03-29 16:21:33 -0400

    Reset

---
+++ Assets/Scripts/LoadSceneOnButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnButton : MonoBehaviour {
+
+    public KeyCode Key = KeyCode.F5;
+    public string Scene = "main";
+
+    void Update() {
+        if (Input.GetKeyDown(Key)) {
+            SceneManager.LoadScene(Scene);
+        }
+    }
+}
